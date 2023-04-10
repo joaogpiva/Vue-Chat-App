84b3c6b4188be713ed1507c9d2478b3a6f8dbae2
João Gabriel Piva <joaogpiva@gmail.com>
2023-04-10 19:32:06 -0300

    Create visual part of sidebar

---
+++ src/components/ProfileSidebar.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="d-flex flex-column" style="padding: 1rem;">
+        <div>
+            <div class="p-2">
+                <i class="fas fa-user fa-4x" style=""></i>
+            </div>
+            <div class="p-2">
+                {{ name }}
+            </div>
+        </div>
+        <div>
+            <div class="p-2">
+                <button class="btn btn-primary" @click="logout()">Log out</button>
+            </div>
+        </div>  
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import authStore from '@/store';
+
+export default defineComponent({
+    name: "ProfileSidebar",
+    props: {
+        email: String,
+        name: String
+    },
+    methods: {
+        logout(){
+            authStore.dispatch('logout');
+        }
+    }
+})
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,25 +1,32 @@
 <template>
     <div class="d-flex flex-column min-vh-100 justify-content-center align-items-center">
-      Home screen!
-      You are logged in as {{ user.email }}
-      <button @click="logout()">Log out</button>
+      <div class="card d-flex flex-row" style="width: 85vw; height: 80vh;">
+        <div style="border-right: 1px solid gray; width: 20%;">
+          <ProfileSidebar :email="this.user.email" :name="this.user.displayName"/>
+        </div>
+        <div>
+          chat window here
+        </div>
+      </div> 
     </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import authStore from '@/store'
+import authStore from '@/store';
+import ProfileSidebar from '@/components/ProfileSidebar.vue';
 
 export default defineComponent({
   name: 'HomeView',
+  components: {
+    ProfileSidebar
+  },
   data(){
     const data: {user: any} = {user: ""}
     return data;
   },
   methods: {
-    logout(){
-      authStore.dispatch('logout');
-    }
+    
   },
   mounted(){
     this.user = authStore.state.user;
