18eb0759bbb22986491a7d0b97392b5b88e6703a
João Gabriel Piva <joaogpiva@gmail.com>
2023-04-14 20:35:08 -0300

    Add functionality to write and read messages to/from Realtime Database

---
+++ src/components/ChatWindow.vue
@@ -0,0 +1,64 @@
+<template>
+    <div class="d-flex flex-column align-items-center justify-content-center" style="height: 100%;">
+        <div class="flex-grow-1" style="width: 100%;">
+            <ul>
+                <li v-for="(m, index) in messages" :key="index">
+                    {{ index }} - {{ m.authorName }}, {{ m.content }}
+                </li>
+            </ul>
+        </div>
+        <div class="d-flex flex-row" style="height: 10%; width: 100%;">
+            <div class="p-2 flex-grow-1">
+                <input class="form-control" placeholder="Type here..." v-model="currentMessage">
+            </div>
+            <div class="p-2" @click="sendMessage()">
+                <button class="btn btn-primary">Send</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import authStore from '@/store';
+import { database } from '@/firebase';
+import { ref, set, onValue } from "firebase/database";
+
+export default defineComponent({
+    name: 'ChatWindow',
+    data(){
+        type messageObj = { authorId: string, authorName: string, timestamp: string, content: string };
+        const data : { messages: messageObj[], currentMessage: string, currentUser: any } = 
+                     { messages: [], currentMessage: "", currentUser: authStore.state.user! };
+        return data;
+    },
+    created(){
+        onValue(ref(database, '/messages'), (snapshot) => {
+            const data = snapshot.val();
+            this.messages = Object.values(data);
+        });
+    },
+    methods: {
+        sendMessage() : void{
+            const currentDate = new Date();
+            const dateString: string = currentDate.getFullYear() + '_' + 
+                (currentDate.getMonth() + 1).toString().padStart(2, '0') + '_' + 
+                currentDate.getDate() + '_' +
+                currentDate.getHours().toString().padStart(2, '0') + '_' + 
+                currentDate.getMinutes().toString().padStart(2, '0') + '_' + 
+                currentDate.getSeconds().toString().padStart(2, '0');
+            
+            const messageToSend = {
+                authorId: this.currentUser['uid'],
+                authorName: this.currentUser['displayName'],
+                timestamp: dateString,
+                content: this.currentMessage
+            }
+
+            set(ref(database, "messages/" + dateString), messageToSend);
+
+            this.currentMessage = "";
+        }
+    }
+});
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="d-flex flex-column min-vh-100 justify-content-center align-items-center">
       <div class="card d-flex flex-row" style="width: 85vw; height: 80vh;">
-        <div style="border-right: 1px solid gray; width: 20%;">
+        <div style="border-right: 1px solid gray; width: 20%; height: 100%;">
           <ProfileSidebar :email="this.user.email" :name="this.user.displayName"/>
         </div>
-        <div>
-          chat window here
+        <div style="width: 80%; height: 100%;">
+          <ChatWindow />
         </div>
       </div> 
     </div>
@@ -15,11 +15,13 @@
 import { defineComponent } from 'vue';
 import authStore from '@/store';
 import ProfileSidebar from '@/components/ProfileSidebar.vue';
+import ChatWindow from '@/components/ChatWindow.vue';
 
 export default defineComponent({
   name: 'HomeView',
   components: {
-    ProfileSidebar
+    ProfileSidebar,
+    ChatWindow
   },
   data(){
     const data: {user: any} = {user: ""}
