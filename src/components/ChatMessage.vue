5614b70ef1f49d4d22ce9b2fc8d0b94b9afb99c4
João Gabriel Piva <joaogpiva@gmail.com>
2023-05-30 21:56:49 -0300

    Update visuals of ChatMessage component

--- src/components/ChatMessage.vue
+++ src/components/ChatMessage.vue
@@ -1,11 +1,16 @@
 <template>
     <div class="d-flex flex-column p-2" style="width: 100%;">
-        <div class="d-flex flex-row justify-content-between" style="height: 20%;">
-            <div class="align-self-start">
-                <p>{{ authorName }}</p>
+        <div class="d-flex flex-row justify-content-between align-items-end" style="height: 20%;">
+            <div class="d-flex flex-direction-row align-items-center justify-content-center align-self-start">
+                <div>
+                    <i class="fa-solid fa-circle-user fa-2x"></i>
+                </div>
+                <div>
+                    <p>{{ authorName }}</p>
+                </div>
             </div>
             <div class="align-self-end">
-                <p>{{ timestamp }}</p>
+                <p>{{ formatDateTime(timestamp) }}</p>
             </div>
         </div>
         <div class="align-self-start">
@@ -23,6 +28,13 @@ export default defineComponent({
         textContent: String,
         authorName: String,
         timestamp: String
+    },
+    methods: {
+        formatDateTime(str?: string) : string{
+            let arr = str?.split("_");
+            if(!arr){ return ""}
+            return arr[3] + ":" + arr[4] + ":" + arr[5] + " " + arr[2] + "/" + arr[1] + "/" + arr[0];
+        }
     }
 })
 </script>
